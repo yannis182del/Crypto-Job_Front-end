e69e5d3c444f7486e90b66b90b3162567d9c097b
Yannis <Yannis182@outlook.fr>
2019-05-19 17:48:47 +0200

    started flexbox instead of grid in the article component

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import "./Article.css"
+import React from "react";
+import "./Article.css";
+import "./ArticleGrid.css";
 
 const Articles = ({ location }) => (
- <div className="article-container">
-<h1>{location.state.workplace_name}</h1>
-<h2>{location.state.position_name}</h2>
-<h2>{location.state.description}</h2>
-<h2>{location.state.Compensation}</h2>
- </div>
-  )
-  
-  export default Articles;
+    <div>
+  <div className="article-container">
+    <div className="flex-header">
+      <p>{location.state.location}</p>
+      <p>{location.state.contract_type}</p>
+    </div>
+    <div  className="article-title">
+      <img className="article-img" src={location.state.img} alt="logo" />{" "}
+    <p className="article-workplace_name">{location.state.workplace_name}</p>
+    <h2>{location.state.position_name}</h2>
+    </div>
+  </div>
+    <div className="grid">
+    <div className="flex-item ">
+    <div className="item1">
+    <h4>Compensation</h4>
+    <p>{location.state.Compensation}</p>
+    </div>
+    <div className="item2">
+    <h4>Skills</h4>
+    {location.state.skills}
+    </div>
+    </div>
+    <div className="item-description">
+    <h4>Job Description</h4>
+<p>{location.state.description}</p>
+    </div>
+    </div>
+  </div>
+);
+
+export default Articles;
---
+++ src/components/Buttons/ApplyButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ApplyButton(props) {
+  return (
+    <div>
+      <button>{props.text}</button>
+    </div>
+  )
+}
