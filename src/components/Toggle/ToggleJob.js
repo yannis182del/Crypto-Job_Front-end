4eebe1f56dcdb9074c4aa2b3888f1af74d55833c
Yannis <Yannis182@outlook.fr>
2019-07-09 14:49:19 +0200

    minor changes

--- src/components/Toggle/ToggleJob.js
+++ src/components/Toggle/ToggleJob.js
@@ -1,38 +1,36 @@
 import React from "react";
-import "./toggle.scss"
-
+import "./toggle.scss";
 
 class Toggle extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
-  
-    render() {
-      return (
-        <div className="button-wrapper">
-          <div className="slider"></div>
-          <button onClick={this.handleClick} className={this.state.isToggleOn ? 'green' : 'red'}>
-            {this.state.isToggleOn ? 'Remote' : 'Hot'}
-          </button>
-        </div>
-      );
-    }
-  }
-  
-  function Muchrender(props) {
+  state = { isToggleOn: true };
+
+  handleClick = () => {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn
+    });
+  };
+
+  render() {
     return (
-      <div className={props.className}>
-        <Toggle />
+      <div className="button-wrapper">
+        <div className="slider" />
+        <button
+          onClick={this.handleClick}
+          className={this.state.isToggleOn ? "green" : "red"}
+        >
+          {this.state.isToggleOn ? "Hot" : "Remote"}
+        </button>
       </div>
     );
   }
+}
 
-  export default Muchrender
+function Muchrender(props) {
+  return (
+    <div className={props.className}>
+      <Toggle />
+    </div>
+  );
+}
+
+export default Muchrender;
