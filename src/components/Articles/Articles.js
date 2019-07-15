b5f55adb05521483c22dbcfe997f195091c6269e
Yannis <Yannis182@outlook.fr>
2019-07-15 22:29:23 +0200

    Article component is now working

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import ArticleComponent from "./ArticleComponent";
 
-const Articles = ({ location }) => (
-  <ArticleComponent
-    position_name={location.state.position_name}
-    workplace_name={location.state.workplace_name}
-    location={location.state.location}
-    contract_type={location.state.contract_type}
-    description={location.state.description}
-    link={location.state.link}
-  />
-);
 
-export default Articles;
+const query = async (id, onFetchData) => {
+  const res = await fetch(`https://cryptoapiforpersonal.herokuapp.com/job/${id}`);
+  const data = await res.json();
+  onFetchData(data);
+}
+
+const getJobId = () => {
+  const pathname = window.location.pathname.split('/');
+  return pathname[pathname.length - 1];
+}
+
+const Articles = () => {
+  const [job, setJob] = useState(null);
+  useEffect(() => {
+    query(getJobId(), setJob);
+  } ,[]);
+
+  return <>{
+      job
+      ? <ArticleComponent
+        position_name={job.position_name}
+        workplace_name={job.workplace_name}
+      />
+      : <span>loading...</span>
+     }</>
+};
+
+export default Articles
+
+
+
+
+
+
